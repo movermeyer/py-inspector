773b7409d50922783ef041a16d13fed9eec690ad
Márcio Duarte <maethorin@gmail.com>
2015-02-02 14:12:06 -0200

    Versionando

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def read(fname):
 
 setup(
     name='py-inspector',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/maethorin/py-inspector',
     license='MIT',
     description='Possibilita a criação de testes para PEP8 e PyLint que serão executados com nose',
